dd1fc49d4999c3bf7f1822c18c7d6cf94d84b44a
BearerOP <ankity1892003@gmail.com>
2025-10-31 18:43:48 +0530

    backend only

--- mobile-app/yukti/src/screens/Home/HomeScreen.tsx
+++
@@ -1,7 +0,0 @@
-// src/screens/Home/HomeScreen.tsx
-import React from 'react';
-import PollsScreen from '../Polls/PollsScreen';
-
-export default function HomeScreen() {
-  return <PollsScreen />;
-}
